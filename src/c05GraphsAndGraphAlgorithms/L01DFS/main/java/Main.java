fb6acc150a45c1a5aacbbb83a57b567f63a84c3b
Lyubomir Georgiev <lyubo12georgiev@gmail.com>
2022-07-22 14:39:05 -0600

    Algorithms Fundamentals with Java - 05 Graphs and Graph Algorithms - Lab 01 and Lab 2 update.

--- src/c05GraphsAndGraphAlgorithms/L01DFS/main/java/Main.java
+++ src/c05GraphsAndGraphAlgorithms/L01DFS/main/java/Main.java
@@ -73,6 +73,47 @@ public class Main {
     }
 
     public static Collection<String> topSort(Map<String, List<String>> graph) {
-        throw new AssertionError("Not Implemented");
+        Map<String, Integer> dependenciesCount = getDependenciesCount(graph);
+
+        List<String> sorted = new ArrayList<>();
+
+        while (!graph.isEmpty()){
+            String key = graph.keySet()
+                    .stream()
+                    .filter(k -> dependenciesCount.get(k) == 0)
+                    .findFirst()
+                    .orElse(null);
+
+            if (key == null){
+                break;
+            }
+
+            for (String child : graph.get(key)) {
+                dependenciesCount.put(child, dependenciesCount.get(child) - 1);
+            }
+
+            sorted.add(key);
+            graph.remove(key);
+        }
+
+        if (!graph.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        return sorted;
+    }
+
+    private static Map<String, Integer> getDependenciesCount(Map<String, List<String>> graph) {
+        Map<String, Integer> dependenciesCount = new LinkedHashMap<>();
+
+        for (Map.Entry<String, List<String>> node : graph.entrySet()) {
+            dependenciesCount.putIfAbsent(node.getKey(), 0);
+
+            for (String child : node.getValue()) {
+                dependenciesCount.putIfAbsent(child, 0);
+                dependenciesCount.put(child, dependenciesCount.get(child) + 1);
+            }
+        }
+
+        return dependenciesCount;
     }
 }
